d9a5e3ca8bfd4d7d5db1dd5c79fec7ec9750428e
Raul Coelho <raulpereira5@gmail.com>
2023-03-28 16:28:54 -0300

    adding install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp .zshrc ~/.zshrc
+cp .vimrc ~/.vimrc
+cp .bashrc ~/.bashrc
+cp .inputrc ~/.inputrc
+cp .p10k.zsh ~/.p10k.zsh
+cp .config/nvim/init.vim ~/.config/nvim/init.vim
+cp .config/rofi/config.rasi ~/.config/rofi/config.rasi
