efcc8b617f8b72b3d64bc4e3b1478adfc04e321b
Raul Coelho <raulpereira5@gmail.com>
2023-10-16 23:59:14 -0300

    fixing install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ cp .vimrc ~/.vimrc
 cp .bashrc ~/.bashrc
 cp .inputrc ~/.inputrc
 cp .p10k.zsh ~/.p10k.zsh
-cp .config/nvim/init.vim ~/.config/nvim/init.vim
-cp .config/rofi/config.rasi ~/.config/rofi/config.rasi
+mkdir -p ~/.config/nvim && cp .config/nvim/init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.config/rofi && cp .config/rofi/config.rasi ~/.config/rofi/config.rasi
